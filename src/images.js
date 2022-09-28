26a271166334a240c9b66eab3e047dddb637bf24
AngieV1198 <angievarela1998@gmail.com>
2022-09-28 17:23:28 -0500

    df

--- src/images.js
+++ src/images.js
@@ -15,5 +15,6 @@ import img14 from "./components/imagen/Group 34.jpg"
 import img15 from "./components/imagen/Group 188.jpg"
 import img16 from "./components/imagen/Group 6.jpg"
 import img17 from "./components/imagen/Group 41.jpg"
+import img18 from "./components/imagen/la huerta.jpg"
 
-export const collection = [img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11,img12,img13,img14,img15,img16,img17]
+export const collection = [img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11,img12,img13,img14,img15,img16,img17,img18]
